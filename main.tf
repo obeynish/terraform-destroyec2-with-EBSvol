f03281175d9b58395a26df775aa7722a557f7293
obeynish <99975157+obeynish@users.noreply.github.com>
2022-08-04 15:58:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
-1
+resource "aws_instance" "instance" { ..snip .. }
+
+resource "aws_ebs_volume" "data" { ..snip.. }
+
+resource "aws_volume_attachment" "data_att" {
+  device_name  = "/dev/sdf"
+  volume_id    = aws_ebs_volume.data.id
+  instance_id  = aws_instance.instance.id
+}
+
+resource "null_resource" "unmount_data_drive" {
+  triggers = {
+    public_ip = aws_instance.instance.public_ip
+  }
+
+  depends_on = [aws_volume_attachment.data_att, aws_instance.instance]
+
+  provisioner "remote-exec" {
+    when       = destroy
+    on_failure = continue
+    connection {
+      type        = "ssh"
+      agent       = false
+      host        = self.triggers.public_ip
+      user        = "ubuntu"
+      private_key = file(var.key_pair)
+    }
+    inline = [
+      "sudo umount /opt/data",
+      "sudo sed -i '/opt\\/data/d' /etc/fstab"
+    ]
+  }
+}
